1bfca44a5cf346a6bd0744fb069dbd5dc01bf2ec
atharv <veerrana1480@gmail.com>
2026-05-29 19:59:01 +0530

    first commit 0.1

--- code.py
+++ code.py
@@ -66,4 +66,4 @@ async def delete_item(item_id: int):
                 "item": deleted
             }
 
-    raise HTTPException(status_code=404, detail="Item not found")
+    raise HTTPException(status_code=404, detail="Item not there")
